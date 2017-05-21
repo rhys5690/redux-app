ab62fd0f4d4c878f9016a9433bf493272ecd02b4
rhys5690 <rhys5690@gmail.com>
2017-05-21 11:42:06 +1000

    .

---
+++ client/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Link
+} from 'react-router';
+
+const Main = react.createClass({
+  render() {
+    return (
+      <div>
+      <h1 to='/'> Tedogram </h1>
+      </div>
+    )
+  }
+});
+
+export default Main;
