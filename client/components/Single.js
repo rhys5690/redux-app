f876f7e851ec289920e794836a568ae6ea2d61bb
rhys5690 <rhys5690@gmail.com>
2017-05-26 10:08:53 +1000

    added single photo view

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router';
-import propTypes from 'prop-types';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+
+    const postComments = this.props.comments[postId] || [];
+
     return (
-    <div className="single-photo">
-    single
-    </div>
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
+      </div>
     )
   }
 });
