6a1a695331ce0e3f34d739aede5dd650f6a707a0
rhys5690 <rhys5690@gmail.com>
2017-05-29 15:26:58 +1000

    updated photos

--- client/data/posts.js
+++ client/data/posts.js
@@ -1,171 +1,171 @@
 const posts = [
    {
       "code":"BAcyDyQwcXX",
-      "caption":"Lunch #hamont",
+      "caption":"New Born Ted",
       "likes":56,
       "id":"1161022966406956503",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12552326_495932673919321_1443393332_n.jpg"
+      "display_src":"https://ibin.co/w800/3O4uvQ9NHuZq.jpg"
    },
    {
       "code":"BAcJeJrQca9",
-      "caption":"Snow! ⛄️🌨❄️ #lifewithsnickers",
+      "caption":"TED",
       "likes":59,
       "id":"1160844458347054781",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg"
+      "display_src":"https://ibin.co/w800/3O5XwUCp3Ovg.jpg"
    },
    {
       "code":"BAF_KY4wcRY",
-      "caption":"Cleaned my office and mounted my recording gear overhead. Stoked for 2016!",
+      "caption":"Teds first toy!!",
       "likes":79,
       "id":"1154606670337393752",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e35/923995_1704188643150533_1383710275_n.jpg"
+      "display_src":"https://ibin.co/3O5YLmAHX38k.jpg"
    },
    {
       "code":"BAPIPRjQce9",
-      "caption":"Making baby pancakes for one early rising baby. ☕️🍴",
+      "caption":"The stolen sock",
       "likes":47,
       "id":"1157179863266871229",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12407480_1654828594805097_152207166_n.jpg"
+      "display_src":"http://i64.tinypic.com/f2r8yx.jpg"
    },
    {
       "code":"-hZh6IQcfN",
-      "caption":"New Stickers just came in. I'll do another mailing in a few weeks if you want some. #javascript",
+      "caption":"Ted",
       "likes":66,
       "id":"1126293663140399053",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/11875511_1562439187344831_813588280_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4222/34919791696_6b67dcd817_z.jpg"
    },
    {
       "code":"-B3eiIwcYV",
-      "caption":"Tacos for breakfast. I love you Austin. 🇺🇸",
+      "caption":"Ted🇺🇸",
       "likes":33,
       "id":"1117418173361145365",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/11917950_927755223968499_1198055371_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4221/34828203651_ac2ba0d3c0_z.jpg"
    },
    {
       "code":"BAhvZrRwcfu",
-      "caption":"Tried poke for the first time at @pokehbar. Delicious! It's like a bowl of sushi",
+      "caption":"Ted",
       "likes":30,
       "id":"1162418651480049646",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e35/12501993_1504179163220771_2060674913_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4196/34960305525_40e5d6fdbc_z.jpg"
    },
    {
       "code":"BAAJqbOQcW5",
-      "caption":"Brunchin'",
+      "caption":"Ted",
       "likes":40,
       "id":"1152964002473690553",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/1516572_445736812276082_2116173059_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4221/34960316885_34ce4f9efc_z.jpg"
    },
    {
       "code":"_4jHytwcUA",
-      "caption":"2015 can be summed up with one baby and a many lines of code. And sometimes a coding baby. 👶🏼⌨",
+      "caption":"Ted⌨",
       "likes":62,
       "id":"1150824171912152320",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/10723795_1149927178351091_1859033096_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4221/34919822726_dd1a31ff31_b.jpg"
    },
    {
       "code":"_zbaOlQcbn",
-      "caption":"Lekker Chocoladeletter",
+      "caption":"Ted",
       "likes":52,
       "id":"1149382879529256679",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12346073_1035047523184672_768982339_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4273/34919855536_926a87798c_z.jpg"
    },
    {
       "code":"_rmvQfQce8",
-      "caption":"Just discovered the #hamont farmers market has a new ramen place! 🍜",
+      "caption":"Ted",
       "likes":35,
       "id":"1147180903383025596",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12331739_1671776806423597_995664526_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4221/34828203651_ac2ba0d3c0_z.jpg"
    },
    {
       "code":"_ep9kiQcVy",
-      "caption":"⛄️",
+      "caption":"Ted",
       "likes":64,
       "id":"1143535906423162226",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12354078_447337935474115_1484398925_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4199/34960367295_2ca2e640f6_z.jpg"
    },
    {
       "code":"_XpJcrwcSn",
-      "caption":"6 page spread on flexbox in this months netmag!",
+      "caption":"Ted",
       "likes":74,
       "id":"1141561999742846119",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12362588_1688046211438811_1395882545_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4219/34828216781_05eaae3e27_z.jpg"
    },
    {
       "code":"_KnU7MwceA",
-      "caption":"Hanging out in my office waiting for 5:00 beers to come around.",
+      "caption":"Ted",
       "likes":54,
       "id":"1137894817632733056",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12301208_1533749386944985_1334730917_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4274/34150029213_169e874546_z.jpg"
    },
    {
       "code":"_HMejJQcY5",
-      "caption":"Today I learned that a long pull espresso is called a 'lungo'",
+      "caption":"Ted",
       "likes":18,
       "id":"1136932306813044281",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12357319_493317964181479_310198908_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4199/34796505002_53e9952a0c_z.jpg"
    },
    {
       "code":"_Fq2zmwcaz",
-      "caption":"Awesome hand lettered gift from @eunibae and the HackerYou crew.",
+      "caption":"Ted",
       "likes":48,
       "id":"1136502965197194931",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12317458_1692845870986430_331905833_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4251/34150120443_71db75e9f4_z.jpg"
    },
    {
       "code":"_A2r0aQcfD",
-      "caption":"Some serious hardware meet JavaScript hacks going down this week at hackeryou. Excited for demo day!",
+      "caption":"Ted",
       "likes":57,
       "id":"1135147611821557699",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12276809_750065668431999_184252508_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4271/34117248124_973f31722f_z.jpg"
    },
    {
       "code":"-1rhFawccs",
-      "caption":"Some major audio upgrades coming to my next videos 😍",
+      "caption":"Ted",
       "likes":39,
       "id":"1132002270913873708",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12331333_1650987978502155_1162510634_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4275/34828376451_84ed7dfb43_z.jpg"
    },
    {
       "code":"-pjx-gQcVi",
-      "caption":"My baby and me. Thanks to @bearandsparrow for this one.",
+      "caption":"Ted",
       "likes":81,
       "id":"1128590547628442978",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12298962_863814057068027_460827278_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4267/34796571302_b51558e4ce_z.jpg"
    },
    {
       "code":"-oTZ0zQcWt",
-      "caption":"It's too early. Send coffee.",
+      "caption":"Ted",
       "likes":81,
       "id":"1128237044221461933",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12328347_990748230999662_1512917342_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4273/34117239794_8feb6b77dd_z.jpg"
    },
    {
       "code":"-mxKQoQcQh",
-      "caption":"They both have figured it out. #lifewithsnickers",
+      "caption":"Ted",
       "likes":47,
       "id":"1127804966031967265",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtp1/t51.2885-15/e35/12256736_1758525004381641_1136705181_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4219/34796587582_87639a28f7_z.jpg"
    },
    {
       "code":"-fasqlQceO",
-      "caption":"Kaitlin decorated the house for the Christmas. So gezellig! #casabos",
+      "caption":"Ted",
       "likes":46,
       "id":"1125735850454402958",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/12277581_1028556737218368_1184190781_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4273/34919855536_926a87798c_z.jpg"
    },
    {
       "code":"-VBgtGQcSf",
-      "caption":"Trying the new Hamilton Brewery beer. Big fan.",
+      "caption":"Ted",
       "likes":27,
       "id":"1122810327591928991",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12224456_175248682823294_1558707223_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4219/34828216781_05eaae3e27_z.jpg"
    },
    {
       "code":"-FpTyHQcau",
-      "caption":"I'm in Austin for a conference and doing some training. Enjoying some local brew with my baby.",
+      "caption":"Ted",
       "likes":82,
       "id":"1118481761857291950",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/11326072_550275398458202_1726754023_n.jpg"
+      "display_src":"https://farm5.staticflickr.com/4243/34828190921_33b00f8a62_z.jpg"
    }
 ];
 
